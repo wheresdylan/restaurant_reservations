03dc2dcb7fae255e4ffd23ac1c7e8cae304b950c
Dylan Marini <dylan.marini@g.austincc.edu>
2018-08-28 12:52:29 -0500

    update .gets

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.get("/", function (req, res) {
 });
 
 app.get("/tables", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 app.get("/reserve", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 
